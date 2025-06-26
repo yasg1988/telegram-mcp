9b73c82f7db2b56ce8f42b3cea3d0ac095cb0f29
yasg1988 <74084838+yasg1988@users.noreply.github.com>
2025-06-26 07:38:08 +0300

    main.py

--- main.py
+++ main.py
@@ -2460,3 +2460,8 @@ if __name__ == "__main__":
             sys.exit(1)
 
     asyncio.run(main())
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
